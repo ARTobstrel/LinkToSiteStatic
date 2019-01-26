4e33a9b4a2cff74267c6fe4419c372a35d8498bd
Artem Fadeev <artemfap@yandex.ru>
2019-01-27 00:54:09 +0300

    commit init

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+//set port
+let port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+//routes
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.listen(port, function () {
+    console.log("app running");
+});
